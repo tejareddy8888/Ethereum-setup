aef21091fb3d7127a0f0dbf41873040886e81856
Saiteja Reddy Pottanigari <58398337+tejareddy8888@users.noreply.github.com>
2023-05-22 14:30:18 +0200

    fixed setup_lighthouse issue

--- setup_lighthouse.sh
+++ setup_lighthouse.sh
@@ -9,7 +9,7 @@ if [ -d "$lighthouse_path" ]; then
   if [ -z "$(ls -A "$lighthouse_path")" ]; then
     echo "$lighthouse_path is empty, please run the 'make fetch'."
   else
-    # sudo apt install -y git gcc g++ make cmake pkg-config llvm-dev libclang-dev clang protobuf-compiler
+    apt install -y git gcc g++ make cmake pkg-config llvm-dev libclang-dev clang protobuf-compiler
 
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
